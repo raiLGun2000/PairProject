aa8e7b058e8346aecfab21a5a4e5b8a9403e6182
raiLGun2000 <49356018+raiLGun2000@users.noreply.github.com>
2021-03-25 23:35:05 +0800

    实现用户权限以及权限管理功能

--- 221801427/papersearch/backend/controllers/PaperController.php
+++ 221801427/papersearch/backend/controllers/PaperController.php
@@ -1,124 +1,163 @@
 <?php
 
-namespace backend\controllers;
+namespace backend\controllers;
 
-use Yii;
-use common\models\Paper;
-use common\models\PaperSearch;
-use yii\web\Controller;
-use yii\web\NotFoundHttpException;
-use yii\filters\VerbFilter;
+use Yii;
+use common\models\Paper;
+use common\models\PaperSearch;
+use yii\web\Controller;
+use yii\web\ForbiddenHttpException;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 
 /**
- * PaperController implements the CRUD actions for Paper model.
- */
-class PaperController extends Controller
+ * PaperController implements the CRUD actions for Paper model.
+ */
+class PaperController extends Controller
 {
-    /**
-     * @inheritdoc
-     */
-    public function behaviors()
-    {
-        return [
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'delete' => ['POST'],
-                ],
-            ],
-        ];
-    }
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['PAPER'],
+                ],
+            ],
 
-    /**
-     * Lists all Paper models.
-     * @return mixed
-     */
-    public function actionIndex()
-    {
-        $searchModel = new PaperSearch();
-        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+            'access' =>[
+                'class' => AccessControl::className(),
+                'rules' =>
+                    [
+                        [
+                            'actions' => ['index', 'view'],
+                            'allow' => true,
+                            'roles' => ['?'],
+                        ],
+                        [
+                            'actions' => ['ueditor','view', 'index', 'create','update','delete'],
+                            'allow' => true,
+                            'roles' => ['@'],
+                        ],
+                    ],
+            ],
+        ];
+    }
 
-        return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
-        ]);
-    }
+    /**
+     * Lists all Paper models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new PaperSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
-    /**
-     * Displays a single Paper model.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionView($id)
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
-        ]);
-    }
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
 
-    /**
-     * Creates a new Paper model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate()
-    {
-        $model = new Paper();
+    /**
+     * Displays a single Paper model.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        } else {
-            return $this->render('create', [
-                'model' => $model,
-            ]);
-        }
-    }
+    /**
+     * Creates a new Paper model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Paper();
 
-    /**
-     * Updates an existing Paper model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
+        if(!Yii::$app->user->can('createPaper')){
+            throw new ForbiddenHttpException('对不起，你没有进行该操作的权限。');
+        }
+        if ($model->load(Yii::$app->request->paper()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        }
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        } else {
-            return $this->render('update', [
-                'model' => $model,
-            ]);
-        }
-    }
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
 
-    /**
-     * Deletes an existing Paper model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionDelete($id)
-    {
-        $this->findModel($id)->delete();
+    /**
+     * Updates an existing Paper model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate($id)
+    {
 
-        return $this->redirect(['index']);
-    }
+        if(!Yii::$app->user->can('updatePaper')){
+            throw new ForbiddenHttpException('对不起，你没有进行该操作的权限。');
+        }
 
-    /**
-     * Finds the Paper model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return Paper the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($id)
-    {
-        if (($model = Paper::findOne($id)) !== null) {
-            return $model;
-        } else {
-            throw new NotFoundHttpException('The requested page does not exist.');
-        }
-    }
+        $model = $this->findModel($id);
+
+        //$model->update_time = time();
+
+        if ($model->load(Yii::$app->request->paper()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Deletes an existing Paper model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionDelete($id)
+    {
+        if(!Yii::$app->user->can('deletePaper')){
+            throw new ForbiddenHttpException('对不起，你没有进行该操作的权限。');
+        }
+
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Paper model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Paper the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Paper::findOne($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+
 }
+
+
