186414bda6abe565128fd122898b0e2e289baeaa
raiLGun2000 <49356018+raiLGun2000@users.noreply.github.com>
2021-03-31 21:52:08 +0800

    手动引入querylist3，编写简单的爬取，结果展示

--- 221801427&221801413/papersearch/frontend/views/site/contact.php
+++ 221801427&221801413/papersearch/frontend/views/site/contact.php
@@ -8,6 +8,10 @@ use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use yii\captcha\Captcha;
 
+//require 'phpQuery.php';
+//require 'QueryList.php';
+//use QL\QueryList;
+
 $this->title = 'Contact';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
@@ -43,3 +47,38 @@ $this->params['breadcrumbs'][] = $this->title;
     </div>
 
 </div>
+
+<?php //以下实现简单的爬取事件
+// require 'phpQuery.php';
+// require 'QueryList.php';
+// use \QL\QueryList;
+// error_reporting(1);
+// 
+// ini_set('max_execution_time', '0');
+//  
+// $html = "https://openaccess.thecvf.com/CVPR2018?day=2018-06-19" ;
+// $baseUrl = "https://openaccess.thecvf.com/" ;
+// 
+// $data = QueryList::Query($html,array(
+    // "ptitle"=>array('.ptitle>a', 'href','',function($content) use($baseUrl){
+        // return $baseUrl.$content;
+    // }),
+// ))->getData(function($item){
+// 
+// $item['ptitle'] = QueryList::Query($item['ptitle'],array(
+        // "title" => array('#papertitle','text'),
+        // 'authors' =>array('#authors','text'),
+        // 'abstract' =>array('#abstract','text'),
+    // ))->data;
+// return $item;
+// 
+// });
+// 
+// foreach ($data as $arr) {
+    // echo "title:";print_r($arr['ptitle'][0]['title']); echo "<br/>";echo "<br/>";
+    // echo "authors:";print_r($arr['ptitle'][0]['authors']); echo "<br/>";echo "<br/>";
+    // echo "abstract:";print_r($arr['ptitle'][0]['abstract']); echo "<br/>";echo "<br/>";echo "<br/>";
+// } 
+// 
+// exit;
+?>
